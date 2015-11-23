c1cbd503ecd3154b00e70c708b78ca2855cc644a
Drew <drew.cooper@sfcta.org>
2015-11-23 14:09:31 -0800

    Commit working FareParser and Fares with ODFare, XFFare and FarelinkFare classes to correspond with CHAMP fare types. Also includes script to test fares.
    Signed-off-by: Drew <drew.cooper@sfcta.org>

--- Wrangler/Fare.py
+++ Wrangler/Fare.py
@@ -3,6 +3,7 @@ import itertools
 from .NetworkException import NetworkException
 from .Node import Node
 from .Logger import WranglerLogger
+from .TransitLink import TransitLink
 
 __all__ = ['Fare']
 
@@ -39,7 +40,32 @@ TRN_MODE_DICT = {1:{'desc':"walk access", 'type':"non-transit"},
                  31:{'desc':"Ferry", 'type':"Ferry"},
                  32:{'desc':"BART", 'type':"BART"},
                  }
-FIRSTBOARD_TYPES = ['non-transit']
+
+NONTRANSIT_TYPES    = ['non-transit','']
+TRANSIT_TYPES       = ['local bus', 'LRT', 'BRT', 'Premium', 'Ferry', 'BART']
+OPERATOR_ID_TO_NAME = {'101': "caltrain", '102': "amtrak", '103': "amtrak", '104': "ace",
+                        '105': "dumbarton", '106': "smart", '107_': "bart", '108_': "bart",
+                        '100_': "bart", '10_': "west_berkeley_shuttle", '11_': "broadway_shuttle",
+                        '12_': "shuttle", '13_': "shuttle", '14_': "caltrain_shuttle",
+                        '15_': "shuttle", '16_': "shuttle", '17_': "shuttle", '18_': "shuttle",
+                        '19_': "shuttle", '26_': "samtrans", '27_': "samtrans", '28_': "samtrans",
+                        '29_': "samtrans", '30_': "samtrans", '31_': "scvta", '32_': "scvta",
+                        '33_': "scvta", '34_': "scvta", '35_': "scvta",
+                        '37_': "ac_transit", '38_': "ac_transit", '39_': "ac_transit", '40_': "ac_transit",
+                        '42_': "lavta", '43_': "lavta", '44_': "lavta", '45_': "lavta",
+                        '47_': "union_city_transit",
+                        '49_': "airbart",
+                        '51_': "cccta", '52_': "cccta", '54_': "tri_delta_transit",
+                        '56_': "westcat", '57_': "westcat", '59_': "vallejo_transit", '60_': "vallejo_transit",
+                        '62_': "fast", '63_': "fast", '64_': "fast",
+                        '65_': "american_canyon", '66_': "vacaville", '68_': "benicia",
+                        '70_': "vine", '71_': "vine",
+                        '73_': "sonoma_county_transit", '74_': "sonoma_county_transit",
+                        '76_': "santa_rosa", '78_': "petaluma",
+                        '80_': "golden_gate_transit", '82_': "golden_gate_transit", '83_': "golden_gate_transit",
+                        '84_': "golden_gate_transit",
+                        '90_': "ferry", '91_': "ferry", '92_': "ferry", '93_': "ferry", '94_': "ferry", '95_': "ferry",
+                        'EBA': "ebart", 'MUN': "sf_muni", 'PRES': "presidigo", 'SFS': "sfsu_shuttle",}
 ##BOARDING_MODEPAIRS
 ##XFER_MODEPAIRS
 
@@ -48,10 +74,34 @@ class Fare(object):
     Fare. Behaves like a dictionary of attributes.
     """
     
-    def __init__(self, name=None, template=None):
+    def __init__(self, name=None, price=None, tod=None, transfers=None, transfer_duration=None, start_time=None, end_time=None, template=None):
         self.attr = {}
+        self.name = name
 
+        # stuff needed for FT
+        self.operator           = None
+        self.fair_id            = None          # calculated
+        self.fare_class         = None          # calculated
+        self.price              = int(price)    # passed by argument
+        self.currency_type      = 'USD'         # default value
+        self.payment_method     = 0             # 0 = on board, 1 = before boarding
+        self.transfers          = transfers     # (0, 1, 2, empty).  Number of transfers permitted on this fare
+        self.transfer_duration  = transfer_duration # OPTIONAL. Leng of time in seconds before transfer expires
+        self.tod                = tod
+        self.start_time         = start_time    # hhmmss, 000000 - 235959
+        self.end_time           = end_time      # hhmmss, 000000 - 235959
 
+    def set_operator(self, champ_operator_id=None, champ_line_name=None):
+        if champ_operator_id:
+            if champ_operator_id in OPERATOR_ID_TO_NAME.keys():
+                self.operator = OPERATOR_ID_TO_NAME[champ_operator_id]
+            elif champ_operator_id + '_' in OPERATOR_ID_TO_NAME.keys():
+                self.operator = OPERATOR_ID_TO_NAME[champ_operator_id + '_']
+            else:
+                raise NetworkException("invalid operator id")
+        elif champ_line_name:
+            prefix_len = champ_line_name.find('_')
+        
     # Dictionary methods
     def __getitem__(self,key): return self.attr[key.upper()]
     def __setitem__(self,key,value): self.attr[key.upper()]=value
@@ -63,100 +113,99 @@ class Fare(object):
         return s
 
     def __str__(self):
-        s = ''
+        s = '%s, $%2f %s' % (self.name if self.name else "unnamed fare", float(price)/100, self.currency_type)
         return s
 
 class ODFare(Fare):
-    def __init__(self, name=None, from_station=None, to_station=None, cost=None, tod=None, start_time=None, stop_time=None, template=None):
-        Fare.__init__(self, name, template)
+    def __init__(self, name=None, from_station=None, to_station=None, price=None, tod=None, start_time=None, end_time=None, template=None, station_lookup=None):
+        Fare.__init__(self, name, price, tod, start_time, end_time, template)
         self.fr     = from_station
         self.to     = to_station
-        self.cost   = cost
-        self.tod    = tod
-
+        
     def __repr__(self):
         pass
 
-class BasicFare(Fare):
-    def __init__(self, name=None, from_mode=None, to_mode=None, cost=None, tod=None, start_time=None, stop_time=None, template=None):
-        Fare.__init__(self, name, template)
+    def __str__(self):
+        pass #s = '' % (
+
+class XFFare(Fare):
+    def __init__(self, name=None, from_mode=None, to_mode=None, price=None, tod=None, transfers=None, transfer_duration=None, start_time=None, end_time=None, template=None):
+        
+        Fare.__init__(self, name, price, tod, transfers, transfer_duration, start_time, end_time, template)
         # stuff from champ
-        self.fr     = from_mode
-        self.to     = to_mode
-        self.fr_desc = Fare.TRN_MODE_DICT[self.fr]['desc']
-        self.to_desc = Fare.TRN_MODE_DICT[self.to]['desc']
-        self.fr_type = Fare.TRN_MODE_DICT[self.fr]['type']
-        self.to_type = Fare.TRN_MODE_DICT[self.to]['type']
-        self.cost   = cost
-        self.tod    = tod
+        self.fr     = int(from_mode)
+        self.to     = int(to_mode)
+        self.fr_desc = TRN_MODE_DICT[self.fr]['desc']
+        self.to_desc = TRN_MODE_DICT[self.to]['desc']
+        self.fr_type = TRN_MODE_DICT[self.fr]['type']
+        self.to_type = TRN_MODE_DICT[self.to]['type']
+        
+        if self.fr_type in NONTRANSIT_TYPES and self.to_type in TRANSIT_TYPES:
+            self.type = 'board'
+            self.fare_id = self.to_type.lower().strip().replace(' ','_')
+        elif self.fr_type in NONTRANSIT_TYPES and self.to_type in NONTRANSIT_TYPES:
+            WrangerLogger.warn('INVALID XFARE TYPE FROM mode %s (%d) to mode %s (%d)' % (self.fr_type, self.fr, self.to_type, self.to))
+        elif self.fr_type in TRANSIT_TYPES and self.to_type in TRANSIT_TYPES:
+            self.type = 'xfer'
+            self.fare_id = self.fr_type.lower().strip().replace(' ','_') + '_to_' + self.to_type.lower().strip().replace(' ','_')
+        else:
+            WranglerLogger.warn('UNKNOWN TRANSIT MODE TYPE (%d, %d)' % (self.fr, self.to))
 
-        # stuff needed for FT (fare_attributes.txt and fare_attributes_ft.txt)
-        self.fare_id            = None #
-        self.fare_class         = None # for _ft.txt
-        self.price              = None
-        self.currency_type      = None
-        self.payment_method     = None # 0 = on board, 1 = before boarding
-        self.transfers          = None # (0, 1, 2, empty).  Number of transfers permitted on this fare
-        self.transfer_duration  = None # OPTIONAL. Leng of time in seconds before transfer expires
-
-        # fare_rules.txt
-        self.fare_id            = None
-        self.route_id           = None
-        self.origin_id          = None
-        self.destination_id     = None
-        self.contains_id        = None
-
-        # fare_rules_ft.txt
-        self.fare_id            = None
-        self.fare_class         = None
-        self.start_time         = None
-        self.end_time           = None
-
-
+        # stuff needed for FT
+        if tod:
+            # if no start/end/tod, then assume all-day fare
+            self.fare_class = self.fare_id + '_' + str(tod).lower()
+        elif start_time and end_time:
+            self.fare_class = self.fare_id + '_' + left(str(start_time),4)
+        else:
+            self.fare_class = self.fare_id + '_allday'
 
     def __repr__(self):
-        s = ''
-        for key in self.keys:
-            if self[key] != None:
-                s = s + ', %s: %s' % (key, self[key])
+        #s = '%s, %s (%d), %s (%d), %d' % (self.name, self.fr_desc, self.fr, self.to_desc, self.to, self.cost)
+        s = '%s, %s, %s, %d' % (self.fare_id, self.fr_desc, self.to_desc, self.price)
         return s
 
-##    def write
+    def __str__(self):
+        #s = '%s, %s (%d), %s (%d), %d' % (self.name, self.fr_desc, self.fr, self.to_desc, self.to, self.cost)
+        s = '%s, %s, %s, %d' % (self.fare_id, self.fr_desc, self.to_desc, self.price)
+        return s
 
-class TransferFare(Fare):
-    def __init__(self, name=None, from_mode=None, to_mode=None, cost=None, tod=None, start_time=None, stop_time=None, template=None):
-        Fare.__init__(self, name, template)
-        self.fr     = from_mode
-        self.to     = to_mode
-        self.fr_desc = Fare.TRN_MODE_DICT[self.fr]['desc']
-        self.to_desc = Fare.TRN_MODE_DICT[self.to]['desc']
-        self.fr_type = Fare.TRN_MODE_DICT[self.fr]['type']
-        self.to_type = Fare.TRN_MODE_DICT[self.to]['type']
-        self.cost   = cost
-        self.tod    = tod
-
-        # fare_transfer_rules.txt
-        self.from_fare_class    = None
-        self.to_fare_class      = None
-        self.is_flat_fee        = None
-        self.transfer_rule      = None
-
-        # transfers.txt
-        self.from_stop_id       = None
-        self.to_stop_id         = None
-        self.transfer_type      = None
-        self.min_transfer_time  = None
-
-        # transfers_ft.txt
-        self.from_stop_id           = None
-        self.to_stop_id             = None
-        self.dist                   = None
-        self.from_route_id          = None
-        self.to_route_id            = None
-        self.schedule_precedence    = None # either 'from' or 'to'
-        self.elevation_gain         = None
-        self.population_density     = None
-        self.retail_density         = None
-        self.auto_capacity          = None
-        self.indirectness           = None
+class FarelinksFare(Fare):
+    def __init__(self, name=None, links=None, modes=None, price=None, tod=None, start_time=None, end_time=None, template=None):
+        Fare.__init__(self, name, price, tod, start_time, end_time, template)
+        self.fare_id = 'farelink_%s_%s' % (str(links), str(modes))
+        self.price = int(price)
+        self.modes = modes if isinstance(modes, list) else None
+        self.farelinks = []
         
+        if isinstance(links, list):
+            for l in links:
+                link = TransitLink()
+                link.setId(l)
+                self.farelinks.append(link)
+        else:
+            link = TransitLink()
+            link.setId(links)
+            self.farelinks.append(link)
+
+    def __repr__(self):
+        s = '%s, $%.2f, links:' % (self.fare_id, self.price/100)
+        for link in self.farelinks:
+            s += ',%s-%s' % (str(link.nodeA), str(link.nodeB))
+
+        s += ', modes:'
+        for mode in self.modes:
+            s += ',%s' % str(mode)
+            
+        return s
+
+    def __str__(self):
+        s = '%s, $%.2f, links:' % (self.fare_id, self.price/100)
+        for link in self.farelinks:
+            s += ',%s-%s' % (str(link.Anode), str(link.Bnode))
+
+        s += ', modes:'
+        for mode in self.modes:
+            s += ',%s' % str(mode)
+            
+        return s        
--- Wrangler/FareParser.py
+++ Wrangler/FareParser.py
@@ -4,7 +4,7 @@ from simpleparse.parser import Parser
 from simpleparse.dispatchprocessor import *
 import re, math
 
-from .Fare import Fare, BasicFare, TransferFare
+from .Fare import Fare, XFFare, ODFare, FarelinksFare
 
 __all__ = [ 'FareParser' ]
 
@@ -14,36 +14,50 @@ WRANGLER_FAREFILE_SUFFICES = [ "fare" ]
 # NOTE: even though XYSPEED and TIMEFAC are node attributes here, I'm not sure that's really ok --
 # Cube documentation implies TF and XYSPD are node attributes...
 fare_file_def=r'''
-fare_file         := ( od_fare / xf_fare )+, smcw*, whitespace*
+fare_file         := ( od_fare / xf_fare / farelinks_fare)+, smcw*, whitespace*
 
 od_fare           := whitespace?, smcw?, nodepair, whitespace?, cost, whitespace?, semicolon_comment*
 xf_fare           := whitespace?, smcw?, fareblock, whitespace?, "=", whitespace?, cost, whitespace?, semicolon_comment*
-
-nodenum           := int
+farelinks_fare    := (whitespace?, smcw?, c"FARELINKS FARE", "=", whitespace?, cost, whitespace?, comma, whitespace?, "L=",
+                        whitespace?, nodepairs, comma?, whitespace?, farelinks_attr*, whitespace?, semicolon_comment*)
+farelinks_attr    := (farelinks_attr_name, whitespace?, "=", whitespace?, attr_values, whitespace?, comma?, whitespace?)
+farelinks_attr_name := ( c"modes" / c"oneway" )
 cost              := int
 numseq            := int, (spaces?, ("-" / ","), spaces?, int)*
-nodepair          := nodenum, ((spaces?, ("-" / ","), spaces?) / [\t]), nodenum
+nodepairs         := nodepair, ( whitespace?, comma, whitespace?, nodepair)*
+nodepair          := nodenum_a, ((spaces?, ("-" / ","), spaces?) / [\t] / ' '), nodenum_b
+attr_values       := attr_value, (whitespace?, comma, whitespace?, attr_value)*
 attr_value        := alphanums / string_single_quote / string_double_quote
 alphanums         := [a-zA-Z0-9\.]+
 <comma>           := [,]
 <whitespace>      := [ \t\r\n]+
 <spaces>          := [ \t]+
 smcw              := whitespace?, (semicolon_comment / c_comment, whitespace?)+
-fareblock         := word_xfer, modeblock_a, modeblock_b
-modeblock_a       := openblock, modenum, closeblock
-modeblock_b       := openblock, modenum, closeblock
+fareblock         := word_xfer, openblock, modenum_a, closeblock, openblock, modenum_b, closeblock
+modenum_a         := modenum
+modenum_b         := modenum
 modenum           := int
+nodenum_a         := nodenum
+nodenum_b         := nodenum
+nodenum           := int
 openblock         := "["
 closeblock        := "]"
 word_xfer         := c"XFARE"
+word_mode         := c"mode"
+word_modes        := c"modes"
 '''
 
+
+##farelinks_fare    := (whitespace?, smcw?, c"FARELINKS FARE=", whitespace?, cost, whitespace?, comma, whitespace?, "L=",
+##                        whitespace?, nodepair, farelinks_attr*, whitespace?, semicolon_comment*)
 class FareFileProcessor(DispatchProcessor):
     """ Class to process fare files
     """
     def __init__(self, verbosity=1):
         self.verbosity=verbosity
-        self.fares = []
+        self.xf_fares = []
+        self.od_fares = []
+        self.farelinks_fares = []
         self.endcomments = []
 
     def crackTags(self, leaf, buffer):
@@ -67,7 +81,7 @@ class FareFileProcessor(DispatchProcessor):
         # Append list items for this line
         for leaf in subtags:
             xxx = self.crackTags(leaf,buffer)
-            self.fares.append(xxx)
+            self.od_fares.append(xxx)
 
         if self.verbosity==2:
             # lines are composed of smcw (semicolon-comment / whitespace), line_attr and lin_node
@@ -85,7 +99,7 @@ class FareFileProcessor(DispatchProcessor):
         # Append list items for this line
         for leaf in subtags:
             xxx = self.crackTags(leaf,buffer)
-            self.fares.append(xxx)
+            self.xf_fares.append(xxx)
 
         if self.verbosity==2:
             # lines are composed of smcw (semicolon-comment / whitespace), line_attr and lin_node
@@ -94,7 +108,25 @@ class FareFileProcessor(DispatchProcessor):
                 for partpart in farepart[3]:
                     print partpart[0], "(", buffer[partpart[1]:partpart[2]],")",
                 print " ]"
-         
+
+    def farelinks_fare(self, (tag,start,stop,subtags), buffer):
+        # this is the whole line
+        if self.verbosity>=1:
+            print tag, start, stop
+
+        # Append list items for this line
+        for leaf in subtags:
+            xxx = self.crackTags(leaf,buffer)
+            self.farelinks_fares.append(xxx)
+
+        if self.verbosity==2:
+            # lines are composed of smcw (semicolon-comment / whitespace), line_attr and lin_node
+            for farepart in subtags:
+                print "  ",farepart[0], " -> [ ",
+                for partpart in farepart[3]:
+                    print partpart[0], "(", buffer[partpart[1]:partpart[2]],")",
+                print " ]"
+                
     def smcw(self, (tag,start,stop,subtags), buffer):
         """ Semicolon comment whitespace
         """
@@ -115,7 +147,7 @@ class FareParser(Parser):
     def buildProcessor(self):
         return self.ffp
 
-    def convertFareData(self):
+    def convertXFFareData(self):
         """ Convert the parsed tree of data into a usable python list of fares
             returns list of comments and fare objects
         """
@@ -124,10 +156,9 @@ class FareParser(Parser):
         key = None
         value = None
         comment = None
-
-        for fare in self.ffp.fares:
-            # each link is a (key, value) tuple
-            
+        modea, modeb, cost = None, None, None
+        
+        for fare in self.ffp.xf_fares:
             # add comments as simple strings:
             if fare[0] in ('smcw','semicolon_comment'):
                 if currentFare:
@@ -137,51 +168,117 @@ class FareParser(Parser):
                 else:
                     rows.append(fare[1].strip())
                 continue
-            if fare[0] == 'xf_fare':
+            if fare[0] == 'fareblock':
                 if currentFare:
                     rows.append(currentFare)
-                    #currentFare = BasicFare(
-##        rows = []
-##        currentLink = None
-##        key = None
-##        value = None
-##        comment = None
-##
-##        for link in self.tfp.links:
-##            # Each link is a 3-tuple:  key, value, list-of-children.
-##
-##            # Add comments as simple strings:
-##            if link[0] in ('smcw','semicolon_comment'):
-##                if currentLink:
-##                    currentLink.comment = " "+link[1].strip()  # Link comment
-##                    rows.append(currentLink)
-##                    currentLink = None
-##                else:
-##                    rows.append(link[1].strip())  # Line comment
-##                continue
-##
-##            # Link records
-##            if link[0] == 'link_attr':
-##                # Pay attention only to the children of lin_attr elements
-##                kids = link[2]
-##                for child in kids:
-##                    if child[0] in ('link_attr_name','word_nodes','word_modes'):
-##                        key = child[1]
-##                        # If this is a NAME attribute, we need to start a new TransitLink.
-##                        if key in ('nodes','NODES'):
-##                            if currentLink: rows.append(currentLink)
-##                            currentLink = TransitLink() # Create new dictionary for this transit support link
-##
-##                    if child[0]=='nodepair':
-##                        currentLink.setId(child[1])
-##
-##                    if child[0] in ('attr_value','numseq'):
-##                        currentLink[key] = child[1]
-##                continue
-##
-##            # Got something unexpected:
-##            WranglerLogger.critical("** SHOULD NOT BE HERE: %s (%s)" % (link[0], link[1]))
-##
-##        # Save last link too
-##        if currentLink: rows.append(currentLink)
-##        return rows
+                    currentFare = None
+                for kid in fare[2]:
+                    if kid[0] == 'modenum_a': modea = kid[1]
+                    if kid[0] == 'modenum_b': modeb = kid[1]
+            if fare[0] == 'cost':
+                if currentFare:
+                    rows.append(currentFare)
+                    currentFare = None
+                cost = fare[1]
+
+            if modea and modeb and cost:
+                currentFare = XFFare(from_mode=modea, to_mode=modeb, price=cost)
+                ##print "Current Fare: ", str(currentFare)
+                modea, modeb, cost = None, None, None
+
+        if currentFare: rows.append(currentFare)
+        return rows
+    
+    def convertODFareData(self):
+        """ Convert the parsed tree of data into a usable python list of fares
+            returns list of comments and fare objects
+        """
+        rows = []
+        currentFare = None
+        key = None
+        value = None
+        comment = None
+        nodea, nodeb, cost = None, None, None
+        
+        for fare in self.ffp.od_fares:
+            # add comments as simple strings:
+            if fare[0] in ('smcw','semicolon_comment'):
+                if currentFare:
+                    currentFare.comment = " "+fare[1].strip()  # fare comment
+                    rows.append(currentFare)
+                    currentFare = None
+                else:
+                    rows.append(fare[1].strip())
+                continue
+            if fare[0] == 'nodepair':
+                if currentFare:
+                    rows.append(currentFare)
+                    currentFare = None
+                for kid in fare[2]:
+                    if kid[0] == 'nodenum_a': nodea = kid[1]
+                    if kid[0] == 'nodenum_b': nodeb = kid[1]
+            if fare[0] == 'cost':
+                if currentFare:
+                    rows.append(currentFare)
+                    currentFare = None
+                cost = fare[1]
+
+            if nodea and nodeb and cost:
+                currentFare = ODFare(from_station=nodea, to_station=nodeb, price=cost)
+                ##print "Current Fare: ", str(currentFare)
+                nodea, nodeb, cost = None, None, None
+
+        if currentFare: rows.append(currentFare)
+        return rows
+
+    def convertFarelinksFareData(self):
+        """ Convert the parsed tree of data into a usable python list of fares
+            returns list of comments and fare objects
+        """
+        rows = []
+        currentFare = None
+        key = None
+        value = None
+        comment = None
+        cost, modes, nodepairs = None, [], []
+        
+        for fare in self.ffp.farelinks_fares:
+            # add comments as simple strings:
+            if fare[0] in ('smcw','semicolon_comment'):
+                if currentFare:
+                    currentFare.comment = " "+fare[1].strip()  # fare comment
+                    rows.append(currentFare)
+                    currentFare = None
+                else:
+                    rows.append(fare[1].strip())
+                continue
+            if fare[0] == 'nodepairs':
+                if currentFare:
+                    rows.append(currentFare)
+                    currentFare = None
+                for kid in fare[2]:
+                    if kid[0] == 'nodepair': nodepairs.append(kid[1])
+            if fare[0] == 'farelinks_attr':
+                if currentFare:
+                    rows.append(currentFare)
+                    currentFare = None
+                for kid in fare[2]:
+                    if kid[0]=='farelinks_attr_name': key=kid[1]
+                    if kid[0]=='attr_values':
+                        for kidkid in kid[2]:
+                            if kidkid[0]=='attr_value': value = kidkid[1]
+                            if key=='modes': modes.append(value)
+                    if kid[0]=='semicolon_comment': comment = kid[1].strip()
+            if fare[0] == 'cost':
+                if currentFare:
+                    rows.append(currentFare)
+                    currentFare = None
+                cost = fare[1]
+
+            if len(nodepairs) > 0 and len(modes) > 0 and cost:
+                currentFare = FarelinksFare(links=nodepairs, modes=modes, price=cost)
+                #print "Current Fare: ", str(currentFare)
+                cost, modes, nodepairs = None, [], []
+
+        if currentFare: rows.append(currentFare)
+        return rows                 
