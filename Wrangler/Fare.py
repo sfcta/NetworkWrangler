1c86634f08b33a69ffcee56e4aba7889b7b8965b
Drew <drew.cooper@sfcta.org>
2015-12-09 15:02:17 -0800

    Move lookups to WranglerLookups. Revise logic to set fare_id. Added class fastTripsFare.
    Signed-off-by: Drew <drew.cooper@sfcta.org>

--- Wrangler/Fare.py
+++ Wrangler/Fare.py
@@ -5,82 +5,26 @@ from .NetworkException import NetworkException
 from .Node import Node
 from .Logger import WranglerLogger
 from .TransitLink import TransitLink
+from .Regexes import *
+from .WranglerLookups import WranglerLookups
 
 __all__ = ['Fare']
-
-TRN_MODE_DICT = {1:{'desc':"walk access", 'type':"non-transit"},
-                 2:{'desc':"walk egress", 'type':"non-transit"},
-                 3:{'desc':"drive access", 'type':"non-transit"},
-                 4:{'desc':"drive egress", 'type':"non-transit"},
-                 5:{'desc':"transfer", 'type':"non-transit"},
-                 6:{'desc':"drive funnel", 'type':"non-transit"},
-                 7:{'desc':"walk funnel", 'type':"non-transit"},
-                 8:{'desc':"empty", 'type':""},
-                 9:{'desc':"empty", 'type':""},
-                 10:{'desc':"empty", 'type':""},
-                 11:{'desc':"Local Muni", 'type':"local bus"},
-                 12:{'desc':"Express Muni", 'type':"local bus"},
-                 13:{'desc':"BRT Muni", 'type':"local bus"},
-                 14:{'desc':"Muni Cable Car", 'type':"LRT"},
-                 15:{'desc':"LRT Muni", 'type':"LRT"},
-                 16:{'desc':"Free and Open Shuttles", 'type':"local bus"},
-                 17:{'desc':"SamTrans Local", 'type':"local bus"},
-                 18:{'desc':"AC Local", 'type':"local bus"},
-                 19:{'desc':"Other Local MTC Buses", 'type':"local bus"},
-                 20:{'desc':"Regional BRT", 'type':"BRT"},
-                 21:{'desc':"VTA LRT", 'type':"LRT"},
-                 22:{'desc':"AC Transbay Buses", 'type':"Premium"},
-                 23:{'desc':"Golden Gate Bus", 'type':"Premium"},
-                 24:{'desc':"Sam Trans Express Bus", 'type':"Premium"},
-                 25:{'desc':"Other Premium Bus", 'type':"Premium"},
-                 26:{'desc':"Caltrain", 'type':"Premium"},
-                 27:{'desc':"SMART", 'type':"Premium"},
-                 28:{'desc':"eBART", 'type':"Premium"},
-                 29:{'desc':"Regional Rail/ACE/AMTRAK", 'type':"Premium"},
-                 30:{'desc':"HSR", 'type':"Premium"},
-                 31:{'desc':"Ferry", 'type':"Ferry"},
-                 32:{'desc':"BART", 'type':"BART"},
-                 }
-
-NONTRANSIT_TYPES    = ['non-transit','']
-TRANSIT_TYPES       = ['local bus', 'LRT', 'BRT', 'Premium', 'Ferry', 'BART']
-OPERATOR_ID_TO_NAME = {'101': "caltrain", '102': "amtrak", '103': "amtrak", '104': "ace",
-                        '105': "dumbarton", '106': "smart", '107_': "bart", '108_': "bart",
-                        '100_': "bart", '10_': "west_berkeley_shuttle", '11_': "broadway_shuttle",
-                        '12_': "shuttle", '13_': "shuttle", '14_': "caltrain_shuttle",
-                        '15_': "shuttle", '16_': "shuttle", '17_': "shuttle", '18_': "shuttle",
-                        '19_': "shuttle", '26_': "samtrans", '27_': "samtrans", '28_': "samtrans",
-                        '29_': "samtrans", '30_': "samtrans", '31_': "scvta", '32_': "scvta",
-                        '33_': "scvta", '34_': "scvta", '35_': "scvta",
-                        '37_': "ac_transit", '38_': "ac_transit", '39_': "ac_transit", '40_': "ac_transit",
-                        '42_': "lavta", '43_': "lavta", '44_': "lavta", '45_': "lavta",
-                        '47_': "union_city_transit",
-                        '49_': "airbart",
-                        '51_': "cccta", '52_': "cccta", '54_': "tri_delta_transit",
-                        '56_': "westcat", '57_': "westcat", '59_': "vallejo_transit", '60_': "vallejo_transit",
-                        '62_': "fast", '63_': "fast", '64_': "fast",
-                        '65_': "american_canyon", '66_': "vacaville", '68_': "benicia",
-                        '70_': "vine", '71_': "vine",
-                        '73_': "sonoma_county_transit", '74_': "sonoma_county_transit",
-                        '76_': "santa_rosa", '78_': "petaluma",
-                        '80_': "golden_gate_transit", '82_': "golden_gate_transit", '83_': "golden_gate_transit",
-                        '84_': "golden_gate_transit",
-                        '90_': "ferry", '91_': "ferry", '92_': "ferry", '93_': "ferry", '94_': "ferry", '95_': "ferry",
-                        'EBA': "ebart", 'MUN': "sf_muni", 'PRES': "presidigo", 'SFS': "sfsu_shuttle",}
 
 class Fare(object):
     """
     Fare. Behaves like a dictionary of attributes.
     """
     
-    def __init__(self, fare_id=None, price=None, tod=None, transfers=None, transfer_duration=None, start_time=None, end_time=None, template=None):
+    def __init__(self, fare_id=None, operator=None, line=None, price=None, tod=None, transfers=None, transfer_duration=None, start_time=None, end_time=None, champ_line_name=None):
         self.attr = {}
         self.fare_id = fare_id
 
         # stuff needed for FT
-        self.operator           = None
         self.fair_id            = None          # calculated
         self.fare_class         = None          # calculated
+        self.operator           = operator
+        self.line               = line
+        self.champ_line_name    = champ_line_name
         self.price              = int(price)    # passed by argument
         self.currency_type      = 'USD'         # default value
         self.payment_method     = 0             # 0 = on board, 1 = before boarding
@@ -89,28 +33,77 @@ class Fare(object):
         self.tod                = tod
         self.start_time         = start_time    # hhmmss, 000000 - 235959
         self.end_time           = end_time      # hhmmss, 000000 - 235959
+        self.setFareId()
+        self.setFareClass()
 
-    def set_operator(self, champ_operator_id=None, champ_line_name=None):
-        if champ_operator_id:
-            if champ_operator_id in OPERATOR_ID_TO_NAME.keys():
-                self.operator = OPERATOR_ID_TO_NAME[champ_operator_id]
-            elif champ_operator_id + '_' in OPERATOR_ID_TO_NAME.keys():
-                self.operator = OPERATOR_ID_TO_NAME[champ_operator_id + '_']
-            else:
-                raise NetworkException("invalid operator id")
-        elif champ_line_name:
-            prefix_len = champ_line_name.find('_')
-
-    def set_fare_id(self, fare_id=None, style='fasttrips', index=None):
+    def setOperatorAndLineFromChamp(self, champ_line_name=None):
+        linename_dict = linename_pattern.match(name)
+        if not linename_dict: raise NetworkException("INVALID LINENAME %s" % str(name))
+        self.operator   = WranglerLookups.OPERATOR_ID_TO_NAME[linename_dict['operator']]
+        self.line       = WranglerLookups.OPERATOR_ID_TO_NAME[linename_dict['name']]
+            
+    def setFareId(self, fare_id=None, style='fasttrips', suffix=None):
         if fare_id:
             self.fare_id = fare_id
+            return self.fare_id
+        elif self.operator and self.line:
+            operpart = self.operator
+            linepart = self.line
+        elif self.champ_line_name:
+            linename_dict = linename_pattern.match(self.champ_line_name)
+            if not linename_dict: raise NetworkException("INVALID LINENAME %s" % str(self.champ_line_name))
+            operpart = WranglerLookups.OPERATOR_ID_TO_NAME[linename_dict['operator']]
+            linepart = WranglerLookups.OPERATOR_ID_TO_NAME[linename_dict['name']]
         else:
-            self.fare_id = 'basic'
-
-        if index:
-            self.fare_id = '%s_%s' % (self.fare_id, str(index))
+            self.fare_id = 'generic'
+            return self.fare_id
+        
+        self.fare_id    = '%s_%s' % (operpart, linepart)
+        if suffix:
+            self.fare_id = '%s_%s' % (self.fare_id, str(suffix))
             
         return self.fare_id
+
+    def setFareClass(self, fare_class=None, style='fasttrips', suffix=None):
+        if self.fare_id:
+            self.fare_class = self.fare_id
+        else:
+            self.fare_class = self.setFareId()
+        todpart1 = self.convertStringToTimePeriod(self.start_time)
+        todpart2 = self.convertStringToTimePeriod(self.end_time)
+        if todpart1 == todpart2:
+            todpart = todpart1
+        else:
+            todpart = '%s_to_%s' % (todpart1, todpart2)
+        self.fare_class = '%s_%s' % (self.fare_class, todpart)
+        
+    def convertStringToTimePeriod(self, hhmmss):
+        if hhmmss == None:
+            tod = 'allday'
+            return tod
+        
+        re_hhmmss = re.compile('\d\d\d\d\d\d')
+        m = re_hhmmss.match(hhmmss)
+        if not m:
+            raise NetworkException('Invalid timestring format for hhmmss: %s' % str(hhmmss))
+        
+        if hhmmss < '030000':
+            tod = 'ev'
+        elif hhmmss < '060000':
+            tod = 'ea'
+        elif hhmmss < '090000':
+            tod = 'am'
+        elif hhmmss < '153000':
+            tod = 'md'
+        elif hhmmss < '183000':
+            tod = 'pm'
+        elif hhmmss < '240000':
+            tod = 'ev'
+        else:
+            new_hh = '%02d' % (int(hhmmss[:2]) - 24)
+            new_hhmmss = new_hh + hhmmss[2:]
+            tod = convertStringToTimePeriod(new_hhmmss)
+        return tod  
             
     # Dictionary methods
     def __getitem__(self,key): return self.attr[key.upper()]
@@ -127,10 +120,12 @@ class Fare(object):
         return s
 
 class ODFare(Fare):
-    def __init__(self, fare_id=None, from_station=None, to_station=None, price=None, tod=None, start_time=None, end_time=None, template=None, station_lookup=None):
-        Fare.__init__(self, fare_id, price, tod, start_time, end_time, template)
+    def __init__(self, fare_id=None, from_station=None, to_station=None, price=None, tod=None, \
+                 start_time=None, end_time=None, template=None, station_lookup=None):
+        Fare.__init__(self, fare_id=fare_id, price=price, tod=tod, start_time=start_time, end_time=end_time)
         self.fr     = from_station
         self.to     = to_station
+        self.setFareId()
         
     def __repr__(self):
         pass
@@ -139,49 +134,48 @@ class ODFare(Fare):
         pass #s = '' % (
 
 class XFFare(Fare):
-    def __init__(self, fare_id=None, from_mode=None, to_mode=None, price=None, tod=None, transfers=None, transfer_duration=None, start_time=None, end_time=None, template=None):
-        
-        Fare.__init__(self, fare_id, price, tod, transfers, transfer_duration, start_time, end_time, template)
+    def __init__(self, fare_id=None, from_mode=None, to_mode=None, price=None, tod=None, transfers=None, \
+                 transfer_duration=None, start_time=None, end_time=None):
         # stuff from champ
-        self.fr_mode  = int(from_mode)
-        self.to_mode    = int(to_mode)
-        self.fr_desc = TRN_MODE_DICT[self.fr_mode]['desc']
-        self.to_desc = TRN_MODE_DICT[self.to_mode]['desc']
-        self.fr_type = TRN_MODE_DICT[self.fr_mode]['type']
-        self.to_type = TRN_MODE_DICT[self.to_mode]['type']
+        self.fr_mode = int(from_mode)
+        self.to_mode = int(to_mode)
+        self.fr_desc = WranglerLookups.MODE_TO_MODETYPE[self.fr_mode]['desc']
+        self.to_desc = WranglerLookups.MODE_TO_MODETYPE[self.to_mode]['desc']
+        self.fr_type = WranglerLookups.MODE_TO_MODETYPE[self.fr_mode]['type']
+        self.to_type = WranglerLookups.MODE_TO_MODETYPE[self.to_mode]['type']
         
-        if self.fr_type in NONTRANSIT_TYPES and self.to_type in TRANSIT_TYPES:
+        if self.fr_type in WranglerLookups.NONTRANSIT_TYPES and self.to_type in WranglerLookups.TRANSIT_TYPES:
             self.type = 'board'
-            self.fare_id = self.to_type.lower().strip().replace(' ','_')
-        elif self.fr_type in NONTRANSIT_TYPES and self.to_type in NONTRANSIT_TYPES:
+        elif self.fr_type in WranglerLookups.NONTRANSIT_TYPES and self.to_type in WranglerLookups.NONTRANSIT_TYPES:
             WrangerLogger.warn('INVALID XFARE TYPE FROM mode %s (%d) to mode %s (%d)' % (self.fr_type, self.fr, self.to_type, self.to))
-        elif self.fr_type in TRANSIT_TYPES and self.to_type in TRANSIT_TYPES:
+        elif self.fr_type in WranglerLookups.TRANSIT_TYPES and self.to_type in WranglerLookups.TRANSIT_TYPES:
             self.type = 'xfer'
-            self.fare_id = self.fr_type.lower().strip().replace(' ','_') + '_to_' + self.to_type.lower().strip().replace(' ','_')
         else:
             WranglerLogger.warn('UNKNOWN TRANSIT MODE TYPE (%d, %d)' % (self.fr, self.to))
 
-        # stuff needed for FT
-        if tod:
-            # if no start/end/tod, then assume all-day fare
-            self.fare_class = self.fare_id + '_' + str(tod).lower()
-        elif start_time and end_time:
-            self.fare_class = self.fare_id + '_' + left(str(start_time),4)
-        else:
-            self.fare_class = self.fare_id + '_allday'
+        Fare.__init__(self, fare_id=fare_id, price=price, tod=tod, transfers=transfers, \
+                              transfer_duration=transfer_duration, start_time=start_time, end_time=end_time)
+        
+    def setFareId(self, fare_id=None, style='fasttrips', suffix=None):
+        if not suffix: suffix = ''
+        if self.type == 'xfer':
+            suffix = '%s_%s' % (suffix, self.fr_type.lower().strip().replace(' ','_') + \
+                                '_to_' + self.to_type.lower().strip().replace(' ','_'))
+        Fare.setFareId(self,fare_id,style,suffix)
+        
 
     def isBoardType(self):
-        if self.fr_type in NONTRANSIT_TYPES and self.to_type not in NONTRANSIT_TYPES:
+        if self.fr_type in WranglerLookups.NONTRANSIT_TYPES and self.to_type not in WranglerLookups.NONTRANSIT_TYPES:
             return True
         return False
     
     def isTransferType(self):
-        if self.fr_type not in NONTRANSIT_TYPES and self.to_type not in NONTRANSIT_TYPES:
+        if self.fr_type not in WranglerLookups.NONTRANSIT_TYPES and self.to_type not in WranglerLookups.NONTRANSIT_TYPES:
             return True
         return False
 
     def isExitType(self):
-        if self.fr_type not in NONTRANSIT_TYPES and self.to_type in NONTRANSIT_TYPES:
+        if self.fr_type not in WranglerLookups.NONTRANSIT_TYPES and self.to_type in WranglerLookups.NONTRANSIT_TYPES:
             return True
         return False
     
@@ -196,11 +190,10 @@ class XFFare(Fare):
         return s
 
 class FarelinksFare(Fare):
-    def __init__(self, fare_id=None, links=None, modes=None, price=None, tod=None, start_time=None, end_time=None, template=None):
-        Fare.__init__(self, fare_id, price, tod, start_time, end_time, template)
-        #self.fare_id = 'farelink_%s_%s' % (str(links), str(modes))
-        self.price = int(price)
-        if isinstance(modes, list):
+    def __init__(self, fare_id=None, links=None, modes=None, price=None, tod=None, start_time=None, end_time=None):
+        if not modes:
+            self.modes = [mode] if mode else []
+        elif isinstance(modes, list):
             self.modes = modes
         else:
             self.modes = [modes]
@@ -208,7 +201,8 @@ class FarelinksFare(Fare):
         self.farelink = None
         self.mode = None
         self.farelinks = []
-        self.set_fare_id()
+        
+        Fare.__init__(self, fare_id=fare_id, price=price, tod=tod, start_time=start_time, end_time=end_time)
         
         if isinstance(links, list):
             for l in links:
@@ -236,18 +230,18 @@ class FarelinksFare(Fare):
         else:
             return False
                     
-    def set_fare_id(self, fare_id=None, style='fasttrips', index=None):
+    def setFareId(self, fare_id=None, style='fasttrips', suffix=None):
         if fare_id:
             self.fare_id = fare_id
         else:
             # count up transit modes
             i = 0
             for m in self.modes:
-                if int(m) in TRN_MODE_DICT.keys():
-                    if not TRN_MODE_DICT[int(m)] in NONTRANSIT_TYPES:
+                if int(m) in WranglerLookups.MODE_TO_MODETYPE.keys():
+                    if not WranglerLookups.MODE_TO_MODETYPE[int(m)] in WranglerLookups.NONTRANSIT_TYPES:
                         i+=1
                         # if it's a transit mode, grab it in case it's the only one
-                        modepart = TRN_MODE_DICT[int(m)]
+                        modepart = WranglerLookups.MODE_TO_MODETYPE[int(m)]
             if i == 1:
                 pass
             elif i > 1:
@@ -270,38 +264,12 @@ class FarelinksFare(Fare):
                 WranglerLogger.debug("Unknown Fare style %s" % style)
                 self.fare_id = '%s_%s_zonefare' % (modepart, todpart)
                 
-            if index:
-                self.fare_id = '%s_%s' % (self.fare_id, index)
+            if suffix:
+                self.fare_id = '%s_%s' % (self.fare_id, suffix)
                 
             return self.fare_id
-
-    def convertStringToTimePeriod(self, hhmmss):
-        if hhmmss == None:
-            tod = 'allday'
-            return tod
         
-        re_hhmmss = re.compile('\d\d\d\d\d\d')
-        m = re_hhmmss.match(hhmmss)
-        if not m:
-            raise NetworkException('Invalid timestring format for hhmmss: %s' % str(hhmmss))
-        
-        if hhmmss < '030000':
-            tod = 'ev'
-        elif hhmmss < '060000':
-            tod = 'ea'
-        elif hhmmss < '090000':
-            tod = 'am'
-        elif hhmmss < '153000':
-            tod = 'md'
-        elif hhmmss < '183000':
-            tod = 'pm'
-        elif hhmmss < '240000':
-            tod = 'ev'
-        else:
-            new_hh = '%02d' % (int(hhmmss[:2]) - 24)
-            new_hhmmss = new_hh + hhmmss[2:]
-            tod = convertStringToTimePeriod(new_hhmmss)
-        return tod     
+   
                     
     def uniqueFarelinksToList(self):
         '''
@@ -337,3 +305,19 @@ class FarelinksFare(Fare):
             s += ',%s' % str(mode)
             
         return s        
+
+class FastTripsFare(Fare):
+    def __init__(self,fare_id=None,operator=None,line=None,origin_id=None,destination_id=None,\
+                 contains_id=None,fare_class=None,start_time=None,end_time=None,champ_line_name=None):
+        
+        Fare.__init__(self, fare_id=fare_id, operator=operator, line=line, price=price, tod=tod, transfers=transfers, \
+                      transfer_duration=transfer_duration, start_time=start_time, end_time=end_time, \
+                      champ_line_name=champ_line_name)
+        self.origin_id      = origin_id
+        self.destination_id = destination_id
+        self.contains_id    = contains_id
+
+    def __getitem__(self,key): return self[key]
+    def __setitem__(self,key,value): self[key]=value
+    def __cmp__(self,other): return cmp(self.__dict__,other.__dict__)
+    
