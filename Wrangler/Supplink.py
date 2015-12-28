b6f6010c2c2dc4a3bd25a1f687886e21f7336be9
Drew <drew.cooper@sfcta.org>
2015-12-28 09:49:33 -0800

    Add FastTrips classes of Supplinks
    Signed-off-by: Drew <drew.cooper@sfcta.org>

--- Wrangler/Supplink.py
+++ Wrangler/Supplink.py
@@ -1,4 +1,8 @@
+import copy
 from .NetworkException import NetworkException
+from .WranglerLookups import WranglerLookups
+from .Logger import WranglerLogger
+from SkimUtil import Skim, HighwaySkim, WalkSkim
 
 __all__ = ['Supplink']
 
@@ -17,7 +21,7 @@ class Supplink(dict):
              7:"WALK_FUNNEL"}
     MODES_INV = dict((v,k) for k,v in MODES.iteritems())
     
-    def __init__(self):
+    def __init__(self, template=None):
         dict.__init__(self)
         self.id=''  # string, e.g. "1-7719"
         self.comment=None
@@ -26,6 +30,8 @@ class Supplink(dict):
         self.Anode = None
         self.Bnode = None
         self.mode  = None
+        if template:
+            self._applyTemplate(template)
 
     def __repr__(self):
         s = "SUPPLINK N=%5d-%5d " % (self.Anode,self.Bnode)
@@ -41,6 +47,15 @@ class Supplink(dict):
 
         return s
 
+    def _applyTemplate(self, template):
+        self.id = copy.deepcopy(template.id)
+        self.comment = copy.deepcopy(template.comment)
+        self.Anode = copy.deepcopy(template.Anode)
+        self.Bnode = copy.deepcopy(template.Bnode)
+        self.mode  = copy.deepcopy(template.mode)
+        for key in template.keys():
+            self[key] = copy.deepcopy(template[key])
+            
     def setId(self, id):
         self.id = id
         
@@ -119,3 +134,178 @@ class Supplink(dict):
         elif self.isWalkEgress(): self.setMode(Supplink.MODES_INV["WALK_ACCESS"])
         elif self.isDriveAccess(): self.setMode(Supplink.MODES_INV["DRIVE_EGRESS"])
         elif self.isDriveEgress(): self.setMode(Supplink.MODES_INV["DRIVE_ACCESS"])
+
+    def asList(self, *args):
+        result = []
+        for arg in args:
+            result.append(getattr(self,arg))
+        return result
+        
+    def asDataFrame(self, *args):
+        import pandas as pd
+        if args is None:
+            args = ['stop_id','stop_name','stop_lat','stop_lon','zone_id']
+        data = []        
+        for arg in args:
+            data.append(getattr(self,arg))
+
+        df = pd.DataFrame(columns=args,data=[data])
+        return df
+    
+        
+class FastTripsWalkSupplink(Supplink):
+    def __init__(self, walkskims=None, nodeToTaz=None, maxTaz=None, template=None):
+        Supplink.__init__(self,template)
+        # walk_access req'd
+        self.taz = self.Anode
+        self.stop_id = self.Bnode
+        self.dist = None
+
+        # walk_access optional
+        if walkskims and nodeToTaz and maxTaz:
+            self.setAttributes(walkskims,nodeToTaz,maxTaz)
+        else:            
+            self.elevation_gain = None
+            self.population_density = None
+            self.retail_density = None
+            self.auto_capacity = None
+            self.indirectness = None
+
+    def asDataFrame(self, *args):
+        if args is None:
+            args = ['taz','stop_id','dist','elevation_gain','population_density',
+                    'employment_density','retail_density','auto_capacity','indirectness']
+        result = Supplink.asDataFrame(self, *args)
+        return result
+
+    def asList(self, *args):
+        if args is None:
+            args = ['taz','stop_id','dist','elevation_gain','population_density',
+                    'employment_density','retail_density','auto_capacity','indirectness']
+        result = Supplink.asList(self, *args)
+        return result
+    
+    def setAttributes(self, walkskims, nodeToTaz, maxTaz):
+        if isinstance(walkskims, str):
+            walkskims = WalkSkim(file_dir = walkskims)
+        elif not isinstance(walkskims, WalkSkim):
+            raise NetworkException("Unknown skim type %s" % str(walkskims))
+
+        if self.Anode <= maxTaz:
+            oTaz = self.Anode
+        elif self.Anode in nodeToTaz:
+            oTaz = nodeToTaz[self.Anode]
+        else:
+            raise NetworkException("Counldn't find TAZ for node %d in (%d, %d)" % (self.Anode,self.Anode,self.Bnode))
+
+        if self.Bnode <= maxTaz:
+            dTaz = self.Bnode
+        elif self.Bnode in nodeToTaz:
+            dTaz = nodeToTaz[self.Bnode]
+        else:
+            raise NetworkException("Counldn't find TAZ for node %d in (%d, %d)" % (self.Bnode,self.Anode,self.Bnode))
+        
+        self.dist               = walkskims.getWalkSkimAttribute(oTaz,dTaz,"DISTANCE")   # link sum (miles)
+        self.population_density = walkskims.getWalkSkimAttribute(oTaz,dTaz,"AVGPOPDEN")  # average pop/acre
+        self.employment_density = walkskims.getWalkSkimAttribute(oTaz,dTaz,"AVGEMPDEN")  # average employment/acre
+        self.retail_density     = None #walkSkim.getWalkSkimAttribute(oTaz,dTaz,"AVGRETDEN")  # average retail/acre
+        self.auto_capacity      = walkskims.getWalkSkimAttribute(oTaz,dTaz,"AVGCAP")     # average road capacity (vph)
+        self.elevation_gain     = walkskims.getWalkSkimAttribute(oTaz,dTaz,"ABS_RISE")   # link sum when rise > 0 (feet)
+        self.indirectness       = walkskims.getWalkSkimAttribute(oTaz,dTaz,"INDIRECTNESS") # distance divided by rock dove distance
+        
+    
+class FastTripsDriveSupplink(Supplink):
+    def __init__(self, hwyskims=None, pnrNodeToTaz=None, tp=None, template=None):
+        Supplink.__init__(self,template)
+        # drive_access req'd
+        if self.isDriveAccess():
+            self.taz = self.Anode
+            self.lot_id = self.Bnode
+        elif self.isDriveEgress():
+            self.taz = self.Bnode
+            self.lot_id = self.Anode
+        self.setDirection()
+        self.dist = None        # float, miles
+        self.cost = None        # integer, cents
+        self.travel_time = None # float, minutes
+        self.start_time = None  # hhmmss or blank
+        self.end_time = None    # hhmmss or blank
+
+        if hwyskims and tp and pnrNodeToTaz:
+            self.getSupplinkAttributes(hwyskims, pnrNodeToTaz, tp)
+        elif tp:
+            setStartTimeEndTimeFromTimePeriod(tp)
+
+    def setDirection(self):
+        if self.isDriveAccess():
+            self.direction = 'access' # 'access' or 'egress'
+        elif self.isDriveEgress():
+            self.direction = 'egress'
+        else:
+            self.direction = None
+
+    def setStartTimeEndTimeFromTimePeriod(self, tp):
+        '''
+        Uses the timeperiods from WranglerLookups to set time range in HHMMSS format
+        '''
+        if tp not in WranglerLookups.TIMEPERIOD_TO_TIMERANGE.keys():
+            raise NetworkException("Invalid time period %s" % tp)
+        self.start_time = WranglerLookups.TIMEPERIOD_TO_TIMERANGE[tp][0]
+        self.end_time = WranglerLookups.TIMEPERIOD_TO_TIMERANGE[tp][1]
+
+    def getSupplinkAttributes(self, hwyskims, pnrNodeToTaz, tp):
+        if isinstance(tp, str):
+            TIMEPERIOD_STR_TO_NUM = {}
+            for k, v in Skim.TIMEPERIOD_NUM_TO_STR.iteritems():
+                TIMEPERIOD_STR_TO_NUM[v] = k
+            tpnum = TIMEPERIOD_STR_TO_NUM[tp]
+            tpstr = tp
+        elif isinstance(tp, int):
+            tpnum = tp
+            tpstr = Skim.TIMEPERIOD_NUM_TO_STR[tp]
+        else:
+            raise NetworkException("Unknown type for tp %s" % str(tp))
+
+        self.setDirection()
+        if self.direction == 'access':
+            (time,term,dist,btoll,vtoll,dummy,dummy,dummy) = \
+                hwyskims[tpnum].getHwySkimAttributes(origtaz=self.taz, desttaz=pnrNodeToTaz[self.lot_id], mode=HighwaySkim.MODE_STR_TO_NUM["DA"], segdir = 1)
+            cost = btoll + vtoll
+            if dist < 0.01:        
+                # no access -- try toll
+                (time,term,dist,btoll,vtoll,dummy,dummy,dummy) = \
+                    hwyskims[tpnum].getHwySkimAttributes(origtaz=self.taz, desttaz=pnrNodeToTaz[self.lot_id], mode=HighwaySkim.MODE_STR_TO_NUM["TollDA"],segdir = 1)
+                cost = btoll + vtoll
+
+        elif self.direction == 'egress':  
+            (time,term,dist,btoll,vtoll,dummy,dummy,dummy) = \
+                hwyskims[tpnum].getHwySkimAttributes(origtaz=pnrNodeToTaz[self.lot_id], desttaz=self.taz, mode=HighwaySkim.MODE_STR_TO_NUM["DA"], segdir = 1)
+            cost = btoll + vtoll
+                            
+            if dist > 0.01: # no access - try toll
+                (time,term,dist,btoll,vtoll,dummy,dummy,dummy) = \
+                    hwyskims[tpnum].getHwySkimAttributes(origtaz=pnrNodeToTaz[self.lot_id], desttaz=self.taz, mode=HighwaySkim.MODE_STR_TO_NUM["TollDA"], segdir = 1)
+                cost = btoll + vtoll
+
+        self.dist = dist
+        self.cost = cost
+        self.travel_time = time
+        self.setStartTimeEndTimeFromTimePeriod(tpstr)
+
+
+
+
+class FastTripsTransferSupplink(FastTripsWalkSupplink):
+    def __init__(self,walkskims=None, nodeToTaz=None, maxTaz=None, template=None):
+        FastTripsWalkSupplink.__init__(self, walkskims, nodeToTaz, maxTaz, template)
+        # transfer req'd
+        self.from_stop_id = self.Anode
+        self.to_stop_id = self.Bnode
+        self.transfer_type = None       # 0-whatev,1-timed transfer,2-min xfer time,3-not possible
+        self.min_transfer_time = None   # float, minutes
+        # transfer_ft req'd
+        self.from_route_id = None
+        self.to_route_id = None
+        self.schedule_precedence = None # 'from' or 'to'
+
+        
