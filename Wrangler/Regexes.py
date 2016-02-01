1ffe44c86d336e2909a4e87e95c18a8d025d5be6
Drew <drew.cooper@sfcta.org>
2016-02-01 10:10:28 -0800

    Put Regexes in a class to make Override behavior more clear.
    Signed-off-by: Drew <drew.cooper@sfcta.org>

--- Wrangler/Regexes.py
+++ Wrangler/Regexes.py
@@ -1,11 +1,10 @@
 import re
-
-__all__ = [ 'nodepair_pattern', 'git_commit_pattern','linename_pattern']
-
-nodepair_pattern = re.compile('(\d+)[-,\s]+(\d+)')
-git_commit_pattern = re.compile('commit ([0-9a-f]{40}$)')
-##linename_pattern = re.compile('(?P<operator>^([\d]+_|MUN|EBA|PRES|SFS))(?P<line>[a-zA-Z0-9_.]+)')
-allday_pattern = re.compile('(ALL|all|All)[\s\-_]*(day|DAY|Day)?')
-linename_pattern = re.compile('(?P<operator>^([\d]+_|MUN|EBA|PRES|SFS))(?P<line>[a-zA-Z0-9/_.]+?)((?P<direction>WB|SB|NB|EB|I|O|R)?(?P<agency_short>ACE|VTA|AC|MUN|NAP|WC|GG)?)$')
+##__all__ = [ 'nodepair_pattern', 'git_commit_pattern','allday_pattern','linename_pattern']
+class Regexes(object):
+    nodepair_pattern = re.compile('(\d+)[-,\s]+(\d+)')
+    git_commit_pattern = re.compile('commit ([0-9a-f]{40}$)')
+    ##linename_pattern = re.compile('(?P<operator>^([\d]+_|MUN|EBA|PRES|SFS))(?P<line>[a-zA-Z0-9_.]+)')
+    allday_pattern = re.compile('(ALL|all|All)[\s\-_]*(day|DAY|Day)?')
+    linename_pattern = re.compile('(?P<operator>^([\d]+_|MUN|EBA|PRES|SFS))(?P<line>[a-zA-Z0-9/_.]+?)((?P<direction>WB|SB|NB|EB|I|O|R)?(?P<agency_short>ACE|VTA|AC|MUN|NAP|WC|GG)?)$')
 
 
