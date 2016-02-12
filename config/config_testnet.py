02c173c80c666adc527e1c3b81d5f001aa101ff3
Drew <drew.cooper@sfcta.org>
2016-02-12 10:09:31 -0800

    Reorganize config files, change paths to generic.  config_testnet now specifies path to inputs provided with NetworkWrangler
    Signed-off-by: Drew <drew.cooper@sfcta.org>

--- config/config_testnet.py
+++ config/config_testnet.py
@@ -1,22 +1,19 @@
+import sys, os
 # ** VALUES FOR TESTING **
+# <INPUTS>
+CUBE_FREEFLOW                   = os.path.join(os.path.dirname(__file__),r'..\unittests\test_cube_to_fasttrips_input\freeflow_subarea.net')
+HWY_LOADED                      = os.path.join(os.path.dirname(__file__),r'..\unittests\test_cube_to_fasttrips_input')
+TRN_SUPPLINKS                   = None
+TRN_BASE                        = os.path.join(os.path.dirname(__file__),r'..\unittests\test_cube_to_fasttrips_input')
+TRANSIT_CAPACITY_DIR            = os.path.join(os.path.dirname(__file__),r'..\unittests\test_cube_to_fasttrips_input\transit_vehicles')
+CHAMP_NODE_NAMES                = os.path.join(os.path.dirname(__file__),r'..\unittests\test_cube_to_fasttrips_input\nodes.xls')
+MODEL_RUN_DIR                   = None
+OVERRIDE_DIR                    = os.path.join(os.path.dirname(__file__),r'..\unittests\test_cube_to_fasttrips_input\wrangler_overrides')
 
-TIMEPERIODS     = {0:'PM'}
-CUBE_FREEFLOW   = r'Q:\Model Development\SHRP2-fasttrips\Task2\input_champ_network_test\freeflow_subarea.net'
-HWY_LOADED      = r'Q:\Model Development\SHRP2-fasttrips\Task2\input_champ_network_test'
-TRN_LOADED      = r'Q:\Model Development\SHRP2-fasttrips\Task2\input_champ_network_test'
-TRN_BASE        = r'Q:\Model Development\SHRP2-fasttrips\Task2\input_champ_network_test'
-TRANSIT_CAPACITY_DIR = r'Q:\Model Development\SHRP2-fasttrips\Task2\input_champ_network_test\transit_vehicles'
-FT_OUTPATH  = r'Q:\Model Development\SHRP2-fasttrips\Task2\output_fasttrips_networks\testnet'
-CHAMP_NODE_NAMES = r'Q:\Model Development\SHRP2-fasttrips\Task2\input_champ_network_test\nodes.xls'
-MODEL_RUN_DIR = r'X:\Projects\TIMMA\2012_Base_v2'       # for hwy and walk skims
-NODEFILES = {'PM':r'Q:\Model Development\SHRP2-fasttrips\Task2\input_champ_network_test\loaded_highway_data\LOADPM_nodes.csv',}
-LINKFILES = {'PM':r'Q:\Model Development\SHRP2-fasttrips\Task2\input_champ_network_test\loaded_highway_data\LOADPM.csv',}
-OVERRIDE_DIR = r'Q:\Model Development\SHRP2-fasttrips\Task2\input_champ_network_test\wrangler_overrides'
-PSUEDO_RANDOM_DEPARTURE_TIMES = False
-DEPARTURE_TIMES_OFFSET = 0
-sys.path.insert(0,OVERRIDE_DIR)
+# <OUTPUTS>
+FT_OUTPATH                      = os.path.join(os.path.dirname(__file__),r'..\unittests\test_cube_to_fasttrips_output')
 
-##import WranglerLookups as OverrideLookups
-##OVERRIDE_LOOKUPS = copy.deepcopy(OverrideLookups)
-##import Regexes as OverrideRegexes
-##OVERRIDE_REGEXES = copy.deepcopy(OverrideRegexes)
+# <SETTINGS>
+PSUEDO_RANDOM_DEPARTURE_TIMES   = False
+SORT_OUTPUTS                    = True
+DEPARTURE_TIMES_OFFSET          = 0
