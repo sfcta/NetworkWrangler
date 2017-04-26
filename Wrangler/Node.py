bbcc598129b656f1b292b051f2028814e516fb97
Drew <drew.cooper@sfcta.org>
2017-04-26 14:22:52 -0700

    Creates FastTripsPNRNode subclass
    Signed-off-by: Drew <drew.cooper@sfcta.org>

--- Wrangler/Node.py
+++ Wrangler/Node.py
@@ -183,15 +183,14 @@ class FastTripsNode(Node):
     '''
     FastTrips Node Class.
     '''
-    def __init__(self, n, champ_coord_dict=None, stop_lat=None, stop_lon=None, template=None, isPNR=False):
+    def __init__(self, n, champ_coord_dict=None, stop_lat=None, stop_lon=None, template=None):
         Node.__init__(self,n,champ_coord_dict,template)
 
         # stops.txt req'd
         self.stop_id        = abs(int(n))
         self.ispnr          = isPNR
         self.stop_name      = Node.descriptions[self.stop_id] if self.stop_id in Node.descriptions.keys() else str(self.stop_id)
-        if self.ispnr:
-            self.lot_id = 'lot_' + str(self.stop_id)
+            
         self.stop_sequence  = None
         if stop_lat and stop_lon:
             self.stop_lat = stop_lat
@@ -224,7 +223,7 @@ class FastTripsNode(Node):
         self.platform_height        = None
         self.level                  = None
         self.off_board_payment      = None
-
+        
     def addXY(self, coords):
         """
         takes an (x,y) tuple or a dict of node numbers to (x,y) tuples
@@ -249,4 +248,17 @@ class FastTripsNode(Node):
         data = self.asList(columns)        
         df = pd.DataFrame(columns=columns,data=[data])
         return df
+        
+class FastTripsPNRNode(FastTripsNode):
+    def __init__(self, n, champ_coord_dict=None, stop_lat=None, stop_lon=None, template=None, **kwargs):
+        FastTripsNode.__init__(n,champ_coord_dict,stop_lat,stop_lon,template)
+                
+        # PNR attributes
+        self.lot_id         = kwargs['lot_id'] if 'lot_id' in kwargs.keys() else 'lot_' + str(self.stop_id)
+        self.name           = kwargs['name'] if 'name' in kwargs.keys() else None
+        self.capacity       = kwargs['capacity'] if 'capacity' in kwargs.keys() else 1000
+        self.drop_off       = kwargs['drop_off'] if 'drop_off' in kwargs.keys() else None
+        self.hourly_cost    = kwargs['hourly_cost'] if 'hourly_cost' in kwargs.keys() else None
+        self.max_cost       = kwargs['max_cost'] if 'max_cost' in kwargs.keys() else None
+        self.type           = kwargs['type'] if 'type' in kwargs.keys() else None
         
